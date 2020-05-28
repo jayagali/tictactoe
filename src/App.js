1cc02f78e9bf487c7c3ae1223827767b94daf7ee
unknown <galija@yahoo.com>
2020-05-27 18:51:35 -0700

    Added Post changes

--- src/App.js
+++ src/App.js
@@ -119,11 +119,14 @@ class Jotter extends Component {
     //const response =[]
 
     try {
-      const response = await fetch('https://4mryiu5jw5.execute-api.us-west-1.amazonaws.com/Alpha-Dev/alljots?userid=628f634a-0fc8-4ae6-ba7e-eeb3ecb96f0c');
+     // const response = await fetch('https://4mryiu5jw5.execute-api.us-west-1.amazonaws.com/Alpha-Dev/alljots?userid=628f634a-0fc8-4ae6-ba7e-eeb3ecb96f0c');
+     const response = await fetch('https://5rbc6bh8f3.execute-api.us-west-1.amazonaws.com/TestStage/getacct?email=cust_finale@finale.com');
       if (!response.ok) {
+        console.log('Jotter: response info Error:'+response.data)
         throw Error(response.statusText);
       }
       const json = await response.json();
+      console.log('Jotter: response info success:'+response.data)
       console.log('Before calling setState to set to true');
 
       this.setState({ 
@@ -161,13 +164,16 @@ class Jotter extends Component {
 			return <div> Loaded successfully...
         {console.log('Inside loaded successfully')}
         <h1> {this.state.isLoaded} </h1>
+        {console.log ('item:'+items) }
         { items.map(item => (
 						<div key={item.userid}>
 							<li>{item.body}</li>
 							<li>{item.title}</li>
 							<li>{item.jotid}</li>
 						</div>
-					)) }
+        ))
+       
+        }
       </div>;
     } 
     else {
@@ -235,7 +241,9 @@ class Widget extends Component {
 
     try {
      //  const response = await fetch('https://phvk9xwk03.execute-api.us-west-1.amazonaws.com/TestStage//widgetbyid?id=HelloWorld!');
-      const response = await fetch('https://st9ohl4b0d.execute-api.us-west-1.amazonaws.com/DBTest/dbwidget?id=3');
+     // const response = await fetch('https://st9ohl4b0d.execute-api.us-west-1.amazonaws.com/DBTest/dbwidget?id=3');
+      const response = await fetch('https://5rbc6bh8f3.execute-api.us-west-1.amazonaws.com/TestStage/getacct?email=cust_finale@finale.com');
+ 
      
       if (!response.ok) {
         throw Error(response.statusText);
@@ -291,6 +299,8 @@ class Widget extends Component {
           <div key={items.id}>
             <h3> Item id: </h3>{items.id}
             <h3> item Name: </h3>{items.name}
+            <h3> Item id: </h3>{items.id}
+            <h3> item Name: </h3>{items.name}
             <h4> End of message</h4>
           </div>
         /*items.map(item => (
@@ -324,6 +334,269 @@ class Widget extends Component {
 } //end of Widget()
 
 
+class Account extends Component {
+ 
+	constructor(props) {
+    
+    super(props);
+    console.log('hello, Account Here');
+  
+    console.log('inside Account');
+    console.debug('inside Account');
+		this.state = {
+      isLoaded: false,
+      gotResp: 1,
+			items: {},
+		};
+  }
+
+ 
+  async componentDidMount() {
+    console.log('ComponentDidMount got called - in Account Component');
+
+    //fetch('https://api.npms.io/v2/search?q=react')
+    //const response =[]
+
+    try {
+     //  const response = await fetch('https://phvk9xwk03.execute-api.us-west-1.amazonaws.com/TestStage//widgetbyid?id=HelloWorld!');
+     // const response = await fetch('https://st9ohl4b0d.execute-api.us-west-1.amazonaws.com/DBTest/dbwidget?id=3');
+      const response = await fetch('https://5rbc6bh8f3.execute-api.us-west-1.amazonaws.com/TestStage/getacct?email=cust_finale@finale.com');
+ 
+     
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      console.log('Account: Before calling setState to set to true');
+
+      this.setState({ 
+        isloaded: true,
+        gotResp: 2,
+        items: json,
+       });
+       console.log('Account: After calling setState to set to true once');
+       
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log('Account: after setting items to true, twice')
+    
+    console.log('Account: state isLoaded:', this.state.isloaded)
+    console.log('Account: gotResp Value:', this.state.gotResp);
+    console.log('Account: Item Values:', this.state.items);
+   // this.forceUpdate();
+  }
+
+
+
+	render() {
+    
+    console.log('Account: Inside Render:', this.state.isLoaded)
+    console.log('Account: Inside Render:', this.state.gotResp);
+    console.log('Account: Inside Render:', this.state.items)
+    var {isLoaded, items}= this.state;
+
+    /* THIS WORKS
+    return <div className="Jotter">
+        <h1> Inside Render of Jotter </h1>;
+      </div>;
+
+      return <h1> Testing hello </h1>;
+  } 
+   
+}*/
+   
+	//	let { items } = this.state;
+		if (this.state.gotResp == 2) {
+			return <div> Loaded successfully...
+        {console.log('Account: Inside loaded successfully')}
+        <h1> {this.state.isLoaded} </h1>
+        { 
+          <div key={items.id}>
+            <li> Customer Email: </li>{items.email}
+            <li> Customer Last name: </li>{items.lname}
+            <li> Customer First name: </li>{items.fname}
+            <li> Customer Phone: </li>{items.phone}
+            <li> Customer Address: </li>{items.address}
+            <li> Customer Country: </li>{items.country}
+            <li> Customer Zip: </li>{items.zip}
+            <h4> End of message</h4>
+          </div>
+        /*items.map(item => (
+						<div key={item.id}>
+							<li>{item.name}</li>
+						</div>
+        ))*/ }
+        </div>;
+    } 
+    else {
+			return (
+				<div>
+          <h1> Loading...</h1>
+          {
+            console.log('Widget: Inside Else, unsuccessfully')
+          }
+          <h1> {this.state.isLoaded} </h1>
+          {/* items.map(item => 
+            (
+              <div key={item.id}>
+                 <li>{item.id}</li>
+                 <li>{item.widget}</li>
+              </div>
+            )
+            )*/
+          } 
+				</div>
+			);
+		}
+  } // end of render()
+} //end of Account()
+
+
+
+class CreateAcct extends Component {
+ 
+	constructor(props) {
+    
+    super(props);
+    console.log('CreateAcct: hello, CreateAcct Here');
+  
+    console.log('CreateAcct: inside CreateAcct');
+    console.debug('CreateAcct: inside CreateAcct');
+		this.state = {
+      isLoaded: false,
+      gotResp: 1,
+			items: {},
+		};
+  }
+
+ 
+  async componentDidMount() {
+    console.log('CreateAcct: ComponentDidMount got called');
+
+    const accountInfo = {
+      email: 'cool@cool.com',
+      accountType : 'cust_',
+      phone: '650-111-2222',
+      category: 'test'
+    }
+
+    console.log("CreateAcct: account Info stringified: "+ JSON.stringify(accountInfo));
+
+    // POST request using fetch with async/await
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+   //   body: JSON.stringify({ title: 'React POST Request Example' })
+      body: JSON.stringify(accountInfo)
+    };
+    /*
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', requestOptions);
+    const data = await response.json();
+    this.setState({ postId: data.id });
+    */
+
+    try {
+     //  const response = await fetch('https://phvk9xwk03.execute-api.us-west-1.amazonaws.com/TestStage//widgetbyid?id=HelloWorld!');
+     // const response = await fetch('https://st9ohl4b0d.execute-api.us-west-1.amazonaws.com/DBTest/dbwidget?id=3');
+     // const response = await fetch('https://5rbc6bh8f3.execute-api.us-west-1.amazonaws.com/TestStage/getacct?email=cust_finale@finale.com');
+      const response = await fetch(' https://5rbc6bh8f3.execute-api.us-west-1.amazonaws.com/TestStage/createacct', requestOptions);
+     
+      if (!response.ok) {
+        console.log('CreateAcct: response Error:'+response.json);
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      console.log('CreateAcct: Post call successful. Before calling setState to set to true');
+
+      this.setState({ 
+        isloaded: true,
+        gotResp: 2,
+        items: json,
+       });
+       console.log('CreateAcct: After calling setState to set to true once');
+       
+
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log('CreateAcct: after setting items to true, twice')
+    
+    console.log('CreateAcct: state isLoaded:', this.state.isloaded)
+    console.log('CreateAcct: gotResp Value:', this.state.gotResp);
+    console.log('CreateAcct: Item Values:', this.state.items);
+   // this.forceUpdate();
+  }
+
+
+
+	render() {
+    
+    console.log('CreateAcct: Inside Render:', this.state.isLoaded)
+    console.log('CreateAcct: Inside Render:', this.state.gotResp);
+    console.log('CreateAcct: Inside Render:', this.state.items)
+    var {isLoaded, items}= this.state;
+
+    /* THIS WORKS
+    return <div className="Jotter">
+        <h1> Inside Render of Jotter </h1>;
+      </div>;
+
+      return <h1> Testing hello </h1>;
+  } 
+   
+}*/
+   
+	//	let { items } = this.state;
+		if (this.state.gotResp == 2) {
+			return <div> Loaded successfully...
+        {console.log('CreateAcct: Inside loaded successfully')}
+        <h1> {this.state.isLoaded} </h1>
+        { 
+          <div key={items.id}>
+            <li> Customer Email: </li>{items.email}
+            <li> Customer Last name: </li>{items.lname}
+            <li> Customer First name: </li>{items.fname}
+            <li> Customer Phone: </li>{items.phone}
+            <li> Customer Address: </li>{items.address}
+            <li> Customer Country: </li>{items.country}
+            <li> Customer Zip: </li>{items.zip}
+            <h4> End of message</h4>
+          </div>
+        /*items.map(item => (
+						<div key={item.id}>
+							<li>{item.name}</li>
+						</div>
+        ))*/ }
+        </div>;
+    } 
+    else {
+			return (
+				<div>
+          <h1> Loading...</h1>
+          {
+            console.log('CreateAcct: Inside Else, unsuccessfully')
+          }
+          <h1> {this.state.isLoaded} </h1>
+          {/* items.map(item => 
+            (
+              <div key={item.id}>
+                 <li>{item.id}</li>
+                 <li>{item.widget}</li>
+              </div>
+            )
+            )*/
+          } 
+				</div>
+			);
+		}
+  } // end of render()
+} //end of CreateAcct()
+
+
 
 
 
@@ -348,7 +621,8 @@ function App() {
         
         <Game />
         
-        <Widget />
+       
+        <CreateAcct />
 
       </header>
     </div>
